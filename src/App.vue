d3ae284bda913c1de9aedaf16e609154433e5203
Moltus <moltus@protonmail.com>
2023-07-29 17:07:43 +0200

    feat: api request for articles. filter out ones without image. Add 'mark as viewed'. Displays articles as list items

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,29 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref, computed } from 'vue'
+import axios from 'axios'
+
+const articles = ref([])
+
+onMounted(async () => {
+  let result = await axios.get(
+    'https://newsapi.org/v2/everything?q=sports&from=2023-07-01&sortBy=publishedAt&apiKey=3ede5178a73d4c059609dc97b5dc7064'
+  )
+  articles.value = result.data.articles.filter((a) => a.urlToImage)
+  articles.value.forEach((a) => {
+    a.isMarkedAsViewed = false
+  })
+})
+
+const notViewedArticles = computed(() => {
+  console.log(articles.value.filter((a) => !a.isMarkedAsViewed).slice(0, 10))
+  return articles.value.filter((a) => !a.isMarkedAsViewed).slice(0, 10)
+})
+
+function markAsViewed(index) {
+  console.log('mark')
+  articles.value[index].isMarkedAsViewed = true
+}
+</script>
 
 <template>
   <header>
@@ -16,7 +41,12 @@
   </header>
 
   <main>
-    <p>content...</p>
+    <ul>
+      <li v-for="(article, index) in notViewedArticles" :key="index">
+        {{ article.title }}
+        <button @click="markAsViewed(index)">Mark as viewed</button>
+      </li>
+    </ul>
   </main>
 </template>
 
