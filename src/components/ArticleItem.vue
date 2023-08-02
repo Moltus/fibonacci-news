c0392ee810bdc9a41e810de4d5807259844b63ab
Moltus <moltus@protonmail.com>
2023-08-02 15:08:04 +0200

    feat: css transitions for article items

--- src/components/ArticleItem.vue
+++ src/components/ArticleItem.vue
@@ -1,6 +1,5 @@
 <script setup>
 const props = defineProps(['size', 'title', 'image', 'left', 'top'])
-console.log(props)
 </script>
 
 <template>
@@ -8,30 +7,32 @@ console.log(props)
     class="article-item"
     :style="{
       background:
-        'center / contain no-repeat linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.0)), url(' +
-        props.image +
-        ')',
+        // 'center / contain no-repeat linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.0)), url(' +
+        'center/cover no-repeat url(' + props.image + ')',
       width: props.size + 'px',
       height: props.size + 'px',
       top: props.top + 'px',
       left: props.left + 'px',
-      fontSize: props.size / 20 + 'px'
+      fontSize: props.size / 40 + 10 + 'px'
     }"
   >
-    <p>{{ props.title }}</p>
+    <p v-if="props.size > 200">{{ props.title }}</p>
   </div>
 </template>
 
 <style scoped>
 .article-item {
+  transition: all 0.5s ease;
+  color: #ccc;
+  text-shadow: 0 0 5px var(--color-background);
   position: absolute;
   user-select: none;
   box-sizing: border-box;
-  border: solid 1px var(--color-background);
+  border: solid 2px var(--color-background);
 }
 
 .article-item p {
-  padding: 0.5rem;
-  background-color: #00000050;
+  padding: 0.5rem 1rem 1rem 1rem;
+  background-color: #00000060;
 }
 </style>
